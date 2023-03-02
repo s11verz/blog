9469155e665fb74f6b33fbf496bb86aa9a77b48c
Bluebird <72031018+s11verz@users.noreply.github.com>
2023-03-02 14:28:49 +0900

    setState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+/* eslint-disable */
+
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+    let [따봉, 따봉변경] = useState(0);
+    return (
+        <div className='App'>
+            <div className='black-nav'>
+                <h4>ReactBlog</h4>
+            </div>
+
+            <button onClick={() => {
+                let copy = [...글제목];
+                copy = ['강남 우동 맛집', '남자 코트 추천', '파이썬 독학'];
+                글제목변경(copy);
+
+            }}>
+                가나다순
+            </button>
+
+            <button onClick={() => {
+
+                let copy = [...글제목];
+                copy[0] = '여자 코트 추천';
+                글제목변경(copy);
+            }}>글수정</button>
+
+            <div className='list'>
+                <h4>{글제목[0]}
+                    <span onClick={() => 따봉변경(따봉 + 1)}>👍</span>{따봉}
+                </h4>
+                <p>2월 17일 발행</p>
+            </div>
+            <div className='list'>
+                <h4>{글제목[1]}</h4>
+                <p>2월 17일 발행</p>
+            </div>
+
+
+
+            <div className='list'>
+                <h4>{글제목[2]}</h4>
+                <p>2월 17일 발행</p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
