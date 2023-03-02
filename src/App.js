2107102f4177f2dcd04636063d3bd482327b75b6
Bluebird <72031018+s11verz@users.noreply.github.com>
2023-03-02 16:11:50 +0900

    function 분리

--- src/App.js
+++ src/App.js
@@ -7,31 +7,38 @@ function App() {
 
     let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
     let [따봉, 따봉변경] = useState(0);
+
+    function onSortButtonClicked() {
+        let copy = [...글제목];
+        copy = ['강남 우동 맛집', '남자 코트 추천', '파이썬 독학'];
+        글제목변경(copy);
+    }
+
+    function onEditButtonClicked() {
+        let copy = [...글제목];
+        copy[0] = '여자 코트 추천';
+        글제목변경(copy);
+    }
+
+    function onLikeButtonClicked() {
+        따봉변경(따봉 + 1);
+    }
+
     return (
         <div className='App'>
             <div className='black-nav'>
                 <h4>ReactBlog</h4>
             </div>
 
-            <button onClick={() => {
-                let copy = [...글제목];
-                copy = ['강남 우동 맛집', '남자 코트 추천', '파이썬 독학'];
-                글제목변경(copy);
-
-            }}>
+            <button onClick={() => onSortButtonClicked()}>
                 가나다순
             </button>
 
-            <button onClick={() => {
-
-                let copy = [...글제목];
-                copy[0] = '여자 코트 추천';
-                글제목변경(copy);
-            }}>글수정</button>
+            <button onClick={() => onEditButtonClicked()}>글수정</button>
 
             <div className='list'>
                 <h4>{글제목[0]}
-                    <span onClick={() => 따봉변경(따봉 + 1)}>👍</span>{따봉}
+                    <span onClick={() => onLikeButtonClicked()}>👍</span>{따봉}
                 </h4>
                 <p>2월 17일 발행</p>
             </div>
